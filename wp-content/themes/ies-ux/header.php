17fb5ce7d7170069b7365bbf0254dd4c80ca5ae6
viajoker <max.moskovchuk@gmail.com>
2018-11-25 20:11:46 +0200

    stage 2.6.1

--- wp-content/themes/ies-ux/header.php
+++ wp-content/themes/ies-ux/header.php
@@ -45,7 +45,7 @@ $additional_body_class = 'page-' . $current_page;
     <div class="container" style="display: flex;justify-content: flex-end;">
         <div id="google_translate_element"></div><script type="text/javascript">
             function googleTranslateElementInit() {
-                new google.translate.TranslateElement({pageLanguage: 'uk', includedLanguages: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL, autoDisplay: false, gaTrack: true, gaId: 'UA-128945678-1'}, 'google_translate_element');
+                new google.translate.TranslateElement({pageLanguage: 'uk', includedLanguages: 'de,en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL, autoDisplay: false, gaTrack: true, gaId: 'UA-128945678-1'}, 'google_translate_element');
             }
         </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </div>
